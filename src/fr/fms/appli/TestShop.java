9283f9aab189911bca9a30d9ccc82b2314279725
Aurore Chevin <chevinaurore@gmail.com>
2023-03-14 10:34:59 +0100

    classes entities vides

---
+++ src/fr/fms/appli/TestShop.java
@@ -0,0 +1,10 @@
+package fr.fms.appli;
+
+public class TestShop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/fr/fms/dao/CreateConfigFile.java
@@ -0,0 +1,7 @@
+package fr.fms.dao;
+
+public class CreateConfigFile {
+	public static void main(String[] args) {
+		
+	}
+}
